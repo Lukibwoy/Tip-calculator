c75ca3fe028b134d06ae1774502959c698c85ca3
Lukibwoy <lukaszwiczynski@gmail.com>
2023-03-09 18:57:16 +0100

    added function of custom

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 const cash = document.querySelector('.cash')
 const tip = document.querySelectorAll('.tip')
+const custom = document.querySelector('.custom')
 const people = document.querySelector('.people')
 const peopleError = document.querySelector('.people-error')
 const reset = document.querySelector('.reset')
@@ -7,11 +8,13 @@ const totalCash = document.querySelector('.total-cash')
 const amountCash = document.querySelector('.amount-cash')
 
 
+
 const clearAll = () => {
     cash.value = '';
     people.value = '';
     amountCash.textContent = `0.00$`
     totalCash.textContent = `0.00$`
+    custom.value = ''
 }
 
 
@@ -21,7 +24,12 @@ tip.forEach((button) => {
         let newTip = e.target.innerText;
         newTip = newTip.substr(0, newTip.length -1);
         newTip = newTip * 0.01;
-        
+      
+        if(button.classList.contains('custom')){
+         newTip = button.value * 0.01;
+         console.log(newTip);
+        }
+
         calculate(newTip)
         
     })
